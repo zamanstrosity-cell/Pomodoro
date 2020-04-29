271a5b0b1a4d4d98256df0ce21c353522d0eebf8
zamanstrosity-cell <60775072+zamanstrosity-cell@users.noreply.github.com>
2020-04-29 19:08:52 +0200

    timer done, need to figure out implementation

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,23 @@
+//Element selectors
 const settings = document.getElementById("settings-image");
 const menu = document.querySelector(".menu");
 const clock = document.querySelector(".clock-container");
-const timer = document.querySelector(".work-timer");
+const play = document.getElementById("start-image");
+const pause = document.getElementById("pause-image");
+const restart = document.getElementById("restart-image");
+const timerDisplay = document.querySelector(".work-timer");
 const submit = document.querySelector(".submit");
 const pomodoros = document.querySelector("#pomodoros");
 const workTime = document.querySelector("#time");
 const breakTime = document.querySelector("#break");
+const initiate = document.querySelector(".declaration");
+//global variables
+let workInterval, breakInterval, tomatoes, seconds, countdown;
+let isBreak = false;
+let isWork = false;
 
-let workInterval, breakInterval, tomatoes, minutes;
-let seconds = 0;
-function pageState(){ //Sets the page view
+//Sets the page view
+function pageState(){ 
     if (menu.style.visibility == "visible") {
         menu.style.visibility = "hidden";
         clock.style.visibility = "visible"
@@ -20,24 +28,67 @@ function pageState(){ //Sets the page view
     }
 }
 
-settings.addEventListener('click', pageState); //changes view when settingas is clicked
+//Event Listeners
+settings.addEventListener('click', pageState); 
+submit.addEventListener('click', startTimer);
+/* play.addEventListener('click', pauseTimer); */
 
-submit.addEventListener('click', () => { //sets the variables when submit
+
+//Timer function, sets countdown
+function timer(seconds) {
+    clearInterval(countdown);
+
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+        if(secondsLeft < 0){
+            clearInterval(countdown);
+            return;
+        }
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds/ 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes < 10 ? '0' : ''}${minutes}:${remainderSeconds < 10 ? '0': ''}${remainderSeconds}`;
+    timerDisplay.textContent = display;
+}
+
+function startTimer(){
+    pageState();
     workInterval = workTime.value;
     breakInterval = breakTime.value;
     tomatoes = pomodoros.value;
-    pageState();
-    setInterval(countDown, 1000);
-})
-
-function countDown(){
-    minutes = workInterval;
-    if (seconds == 0){
-        minutes--;
-        seconds = 59;
-    }
-    seconds--;
-    timer.innerHTML = `${minutes}:${seconds}`;
+    seconds = workInterval * 60;
+    timer(seconds);
 }
 
 
+
+/* 
+Takes Work Time/Break Time/pomodoros
+
+Declares the session you're in ("work, break")
+
+Display Work Countdown on screen
+
+When paused...pause the clock
+
+when rewind, restarts the clock it's on 
+
+when play, continues the clock
+
+when work interval done, plays alarm, changes to break time(declaration),
+
+appends a pomodoro to the bottom of the screen
+
+when break interval done, plays alarm, starts another work interval
+
+when pomodoros done...plays victory lap..
+*/
